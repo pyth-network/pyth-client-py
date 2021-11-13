e453b144dfb08674c6ff002bfd56d3b97ce19dbc
Daniel Chew <cctdaniel@outlook.com>
2021-11-13 19:12:34 +0000

    enable caplog for loguru

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,8 @@
+import logging
 import pytest
+from _pytest.logging import caplog as _caplog
 from pythclient.solana import SolanaClient
+from loguru import logger
 
 
 @pytest.fixture
@@ -8,3 +11,13 @@ def solana_client():
         endpoint="https://example.com",
         ws_endpoint="wss://example.com",
     )
+
+@pytest.fixture
+def caplog(_caplog):
+    class PropogateHandler(logging.Handler):
+        def emit(self, record):
+            logging.getLogger(record.name).handle(record)
+
+    handler_id = logger.add(PropogateHandler(), format="{message}")
+    yield _caplog
+    logger.remove(handler_id)
