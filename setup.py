d79bd6ff97af4025849488ab7667f204deecedc6
Daniel Chew <cctdaniel@outlook.com>
2022-07-24 14:47:36 +0800

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='pythclient',
-    version='0.1.0',
+    version='0.1.1',
     packages=['pythclient'],
     author='Pyth Developers',
     author_email='contact@pyth.network',
