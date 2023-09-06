57f1fd203a512b2813073721d498d6133b3d2330
Daniel Chew <cctdaniel@outlook.com>
2023-09-06 09:20:21 +0900

    bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='pythclient',
-    version='0.1.12',
+    version='0.1.13',
     packages=['pythclient'],
     author='Pyth Developers',
     author_email='contact@pyth.network',
