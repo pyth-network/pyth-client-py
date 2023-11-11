d6571704433f044dfa6881e7b76f629f6e194482
Daniel Chew <cctdaniel@outlook.com>
2023-11-11 19:52:46 +0900

    fix fx/metal get_next_market_close (#44)
    * fix fx/metal get_next_market_close
    
    * bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='pythclient',
-    version='0.1.16',
+    version='0.1.17',
     packages=['pythclient'],
     author='Pyth Developers',
     author_email='contact@pyth.network',
