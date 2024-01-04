70cabbeef46c6fb10bb3909ce190e287beaad0bb
Daniel Chew <cctdaniel@outlook.com>
2024-01-04 22:26:36 +0900

    support pythtest-conformance and pythtest-crosschain

--- pythclient/utils.py
+++ pythclient/utils.py
@@ -1,36 +1,42 @@
-import ast
-import dns.resolver
-from loguru import logger
 from typing import Optional
+
+from loguru import logger
 
 DEFAULT_VERSION = "v2"
 
 
 # Retrieving keys via DNS TXT records should not be considered secure and is provided as a convenience only.
 # Accounts should be stored locally and verified before being used for production.
-def get_key(network: str, type: str, version: str = DEFAULT_VERSION) -> Optional[str]:
+def get_key(network: str, type: str) -> Optional[str]:
     """
-    Get the program or mapping keys from dns TXT records.
-
-    Example dns records:
-
-        devnet-program-v2.pyth.network
-        mainnet-program-v2.pyth.network
-        testnet-mapping-v2.pyth.network
-        pythnet-mapping-v2.pyth.network
+    Get the program or mapping key.
+    :param network: The network to get the key for. Either "mainnet", "devnet", "testnet", "pythnet", "pythtest-conformance", or "pythtest-crosschain".
+    :param type: The type of key to get. Either "program" or "mapping".
     """
-    url = f"{network}-{type}-{version}.pyth.network"
-    try:
-        answer = dns.resolver.resolve(url, "TXT")
-    except dns.resolver.NXDOMAIN:
-        logger.error("TXT record for {} not found", url)
-        return ""
-    if len(answer) != 1:
-        logger.error("Invalid number of records returned for {}!", url)
-        return ""
-    # Example of the raw_key:
-    #     "program=FsJ3A3u2vn5cTVofAjvy6y5kwABJAqYWpe4975bi2epH"
-    raw_key = ast.literal_eval(list(answer)[0].to_text())
-    # program=FsJ3A3u2vn5cTVofAjvy6y5kwABJAqYWpe4975bi2epH"
-    _, key = raw_key.split("=", 1)
-    return key
+    if network == "pythnet":
+        program_key = "FsJ3A3u2vn5cTVofAjvy6y5kwABJAqYWpe4975bi2epH"
+        mapping_key = "AHtgzX45WTKfkPG53L6WYhGEXwQkN1BVknET3sVsLL8J"
+    elif network == "mainnet":
+        program_key = "FsJ3A3u2vn5cTVofAjvy6y5kwABJAqYWpe4975bi2epH"
+        mapping_key = "AHtgzX45WTKfkPG53L6WYhGEXwQkN1BVknET3sVsLL8J"
+    elif network == "devnet":
+        program_key = "gSbePebfvPy7tRqimPoVecS2UsBvYv46ynrzWocc92s"
+        mapping_key = "BmA9Z6FjioHJPpjT39QazZyhDRUdZy2ezwx4GiDdE2u2"
+    elif network == "testnet":
+        program_key = "8tfDNiaEyrV6Q1U4DEXrEigs9DoDtkugzFbybENEbCDz"
+        mapping_key = "AFmdnt9ng1uVxqCmqwQJDAYC5cKTkw8gJKSM5PnzuF6z"
+    elif network == "pythtest-conformance":
+        program_key = "8tfDNiaEyrV6Q1U4DEXrEigs9DoDtkugzFbybENEbCDz"
+        mapping_key = "AFmdnt9ng1uVxqCmqwQJDAYC5cKTkw8gJKSM5PnzuF6z"
+    elif network == "pythtest-crosschain":
+        program_key = "gSbePebfvPy7tRqimPoVecS2UsBvYv46ynrzWocc92s"
+        mapping_key = "BmA9Z6FjioHJPpjT39QazZyhDRUdZy2ezwx4GiDdE2u2"
+    else:
+        raise Exception(f"Unknown network: {network}")
+
+    if type == "program":
+        return program_key
+    elif type == "mapping":
+        return mapping_key
+    else:
+        raise Exception(f"Unknown type: {type}")
